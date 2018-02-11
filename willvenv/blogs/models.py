da4e8d9b8fc198a159d14f539a95ba9f650897cd
Will Worth <will.worth@gmail.com>
2018-02-11 16:38:44 +0100

    add blog post functionality added

--- willvenv/blogs/models.py
+++ willvenv/blogs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey(
--- willvenv/blogs/views.py
+++ willvenv/blogs/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-
+from django.views.generic.edit import CreateView
 from . models import Post
 
 
@@ -11,3 +11,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
